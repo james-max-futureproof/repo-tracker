813d15a6b56de6fd2b422e082b557f176b31cff9
Velo <hartley.max@outlook.com>
2021-08-10 18:05:26 +0100

    add initial styling to user list component

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UserInfo } from '../';
+import './style.css';
 
 export default ({ userData, loading, error, noUsersFound }) => {
 	const users = userData ? userData.map((user, i) => <UserInfo userData={user} key={i} />) : '';
@@ -12,6 +13,8 @@ export default ({ userData, loading, error, noUsersFound }) => {
 				<p>Loading...</p>
 			) : noUsersFound ? (
 				<p>No users found. Try again with a different username.</p>
+			) : users.length === 0 ? (
+				<p>Search for a Github user...</p>
 			) : (
 				users
 			)}
