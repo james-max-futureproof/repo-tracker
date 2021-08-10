ff18d02329e911e12e895182d39e810e57e3aff3
Velo <hartley.max@outlook.com>
2021-08-10 16:20:09 +0100

    add error handling functionality to landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -4,12 +4,21 @@ import { UserSearch, UserList } from '../../components';
 
 export default () => {
 	const [userData, setUserData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
 
 	const onSubmit = async (userName) => {
-		const { data } = await axios.get(
-			`https://api.github.com/search/users?q=${userName}&per_page=10`
-		);
-		setUserData(data.items);
+		try {
+			setLoading(true);
+			const { data } = await axios.get(
+				`https://api.github.com/search/users?q=${userName}&per_page=10`
+			);
+			setUserData(data.items);
+			setLoading(false);
+		} catch (err) {
+			setError(true);
+			setLoading(false);
+		}
 	};
 
 	return (
@@ -17,7 +26,7 @@ export default () => {
 			<h1>Github Repo Tracker</h1>
 			<p>A simple way to find information about github repositories</p>
 			<UserSearch onSubmit={onSubmit} />
-			<UserList />
+			<UserList userData={userData} error={error} loading={loading} />
 		</main>
 	);
 };
