ab7a9ae04a28bef27f9644e33162d91ac0a5db2c
Velo <hartley.max@outlook.com>
2021-08-10 16:55:00 +0100

    add empty search handling and key in userinfo component

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { UserInfo } from '../';
 
-export default ({ userData, loading, error }) => {
-	const users = userData ? userData.map((user) => <UserInfo userData={user} />) : '';
+export default ({ userData, loading, error, noUsersFound }) => {
+	const users = userData ? userData.map((user, i) => <UserInfo userData={user} key={i} />) : '';
 
 	return (
 		<ul>
@@ -10,7 +10,7 @@ export default ({ userData, loading, error }) => {
 				<p>Oops! Something went wrong...</p>
 			) : loading ? (
 				<p>Loading...</p>
-			) : userData.length === 0 ? (
+			) : noUsersFound ? (
 				<p>No users found. Try again with a different username.</p>
 			) : (
 				users
