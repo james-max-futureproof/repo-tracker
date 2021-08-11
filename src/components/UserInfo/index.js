3f89aacd2e4357275e9017ac50c9851a0b6a707d
Velo <hartley.max@outlook.com>
2021-08-11 10:24:15 +0100

    refactor user list to render hr as user info child

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import './style.css';
 
-export default ({ userData }) => {
+export default ({ userData, children }) => {
 	const { push } = useHistory();
 	const handleClick = () => {
 		push(`/profile/${userData.login}`);
 	};
 	return (
-		<li>
-			<h2>{userData.login}</h2>
-			<button onClick={handleClick}>See Profile</button>
-			<img src={userData.avatar_url} alt="" />
-		</li>
+		<>
+			<li>
+				<h2>{userData.login}</h2>
+				<button onClick={handleClick}>See Profile</button>
+				<img src={userData.avatar_url} alt="" />
+			</li>
+			{children}
+		</>
 	);
 };
