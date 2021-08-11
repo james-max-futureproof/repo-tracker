16ecd6c3d6f9643c02ed25c47e3abc1ba9aa68f1
Velo <hartley.max@outlook.com>
2021-08-11 09:48:28 +0100

    add repo data fetching to repo page

--- src/pages/Repo/index.js
+++ src/pages/Repo/index.js
@@ -6,21 +6,42 @@ import './style.css';
 
 export default () => {
 	const [profileData, setProfileData] = useState([]);
+	const [repoData, setRepoData] = useState([]);
+	const [isRepoDataLoading, setIsRepoDataLoading] = useState(true);
+	const [isRepoError, setIsRepoError] = useState(false);
 
 	const { username, repo } = useParams();
 	useEffect(() => {
 		let isLoading = true;
-		const fetchData = async () => {
+		const fetchUserData = async () => {
 			try {
 				const { data } = await axios.get(`https://api.github.com/users/${username}`);
-				console.log(data);
 				if (isLoading) setProfileData(data);
 			} catch (err) {
 				console.log(err);
 			}
 		};
-		fetchData();
 
+		const fetchRepoData = async () => {
+			try {
+				setIsRepoError(false);
+				setIsRepoDataLoading(true);
+				const { data } = await axios.get(`https://api.github.com/repos/${username}/${repo}`);
+				if (data.message && data.message === 'Not found') {
+					throw new Error(data.message);
+				}
+				console.log(data);
+				setIsRepoDataLoading(false);
+				setRepoData(data);
+			} catch (err) {
+				setIsRepoDataLoading(false);
+				setIsRepoError(true);
+				console.log(err);
+			}
+		};
+
+		fetchUserData();
+		fetchRepoData();
 		return () => {
 			isLoading = false;
 		};
